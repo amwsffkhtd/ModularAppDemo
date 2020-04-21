0e8ac4c6c916449192ab66685753f57440701d81
amwsffkhtd <ayuhuang163@163.com>
2020-04-21 21:31:15 +0800

    add isModule

---
+++ features/dashboard/src/main/java/per/android/study/dashboard/MainDashboardActivity.java
@@ -0,0 +1,13 @@
+package per.android.study.dashboard;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainDashboardActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_dashboard_activity);
+    }
+}
---
+++ features/login/src/main/java/per/android/study/login/MainLoginActivity.java
@@ -0,0 +1,15 @@
+package per.android.study.login;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_login_activity);
+
+    }
+}
